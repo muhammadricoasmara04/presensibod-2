d5245948b7b173d24d5c2690aea7f50cd96b864b
muhammadricoasmara04 <ricoasmara496@gmail.com>
2024-07-06 23:35:50 +0700

    update features DummyUserSeeder and LoginController-Progress..

---
+++ database/seeders/DummyUserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DummyUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userData = [
+            [
+                'name' => 'Muhammad Ricoasmara',
+                'email' => 'ricoasmara496@gmail.com',
+                'role' => 'superadmin',
+                'password' => bcrypt('04oktober2000')
+            ],
+            [
+                'name' => 'johndoepeserta',
+                'email' => 'johndoe@gmail.com',
+                'role' => 'admin',
+                'password' => bcrypt('123456789')
+            ],
+        ];
+        foreach ($userData as $key => $val) {
+            User::create($val);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('login.index', [
-        'title' => 'Login'
-    ]);
-});
+// Route::get('/', function () {
+//     return view('login.index', [
+//         'title' => 'Login'
+//     ]);
+// });
+
+Route::get('/', [LoginController::class, 'index'])->name('login');
