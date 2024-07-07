231b3566d1fddf4fadb10f3eebbe6f73ffa8d2fa
muhammadricoasmara04 <ricoasmara496@gmail.com>
2024-07-07 20:31:30 +0700

    Update Authtentification

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,34 +1,47 @@
 @extends('layout.main')
 @section('container')
 
-<div class="login">
-         {{-- <img src="" alt="login image" class="login__img"> --}}
-         <form action="" class="login__form">
+    <div class="login">
+        {{-- <img src="" alt="login image" class="login__img"> --}}
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="/" class="login__form" method="POST">
+            @csrf
             <h1 class="login__title">Login</h1>
 
             <div class="login__content">
-               <div class="login__box">
-                  <i class="ri-user-3-line login__icon"></i>
+                <div class="login__box">
+                    <i class="ri-user-3-line login__icon"></i>
 
-                  <div class="login__box-input">
-                     <input type="email" required class="login__input" id="login-email" placeholder="">
-                     <label for="login-email" class="login__label">Email</label>
-                  </div>
-               </div>
+                    <div class="login__box-input">
+                        <input type="email" class="login__input" name="email" id="email" placeholder="" autofocus
+                            required value="{{ old('email') }}">
+                        <label for="email" class="login__label">Email</label>
+                    </div>
+                </div>
 
-               <div class="login__box">
-                  <i class="ri-lock-2-line login__icon"></i>
+                <div class="login__box">
+                    <i class="ri-lock-2-line login__icon"></i>
 
-                  <div class="login__box-input">
-                     <input type="password" required class="login__input" id="login-pass" placeholder=" ">
-                     <label for="login-pass" class="login__label">Password</label>
-                     
-                  </div>
-               </div>
+                    <div class="login__box-input">
+                        <input type="password" class="login__input" name="password" id="password" placeholder="" required>
+                        <label for="password" class="login__label">Password</label>
+
+                    </div>
+                </div>
             </div>
 
             <button type="submit" class="login__button">Login</button>
 
-         </form>
-      </div>
+        </form>
+    </div>
+
+
 @endsection
